7eeb73d4e7e66e805e1b15db47012854aedf4dab
alkhwlyhasan777-coder <alkhwlyhasan777@gmail.com>
2026-03-23 12:46:41 +0200

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -11,66 +11,9 @@ const img = document.createElement("img");
     img.src = "../image-home/img/app.png";
     img.alt = "icon";
 img.classList.add("image");
-section.append(span, img)
-header.append(section)
-const nav = document.createElement("nav")
-    nav.classList.add("nav-rigth")
-    
-const ulNavber = `
-    <ul>
-        <li><a href="./index.html">الصفحه الرئيسيه</a></li>
-        <br>
-        <li><a href="./الخدمات/government.html">خدمات حكوميه</a></li> 
-        <li><a href="./الخدمات/travel-permit.html">جواز سفر</a></li>
-        <li><a href="./الخدمات/visas.html">تاشيرات</a></li>
-        <li><a href="./الخدمات/hotel.html">حجز فنادق</a></li>
-        <li><a href="./الخدمات/flight.html">حجز طيران</a></li>
-        <li><a href="./الخدمات/tasahel.html">حجز موعد تسهيل</a></li>
-        <hr>
-        <li><a href="./about.html">من نحن</a></li>
-        <li><a href="./content.html">تواصل معنا</a></li>
-    </ul>
-`;
-if (window.location.pathname === "/index.html" ) {
-    const ulNavber = `
-    <ul>
-        <li><a href="../index.html">الصفحه الرئيسيه</a></li>
-        <br>
-        <li><a href="./الخدمات/government.html">خدمات حكوميه</a></li> 
-        <li><a href="./الخدمات/travel-permit.html">جواز سفر</a></li>
-        <li><a href="./الخدمات/visas.html">تاشيرات</a></li>
-        <li><a href="./الخدمات/hotel.html">حجز فنادق</a></li>
-        <li><a href="./الخدمات/flight.html">حجز طيران</a></li>
-        <li><a href="./الخدمات/tasahel.html">حجز موعد تسهيل</a></li>
-        <hr>
-        <li><a href="../about.html">من نحن</a></li>
-        <li><a href="../content.html">تواصل معنا</a></li>
-    </ul>
-`;
-nav.insertAdjacentHTML("beforeend", ulNavber);
 
-header.append(nav);
-} else {
-    const ulNavber = `
-    <ul>
-        <li><a href="../index.html">الصفحه الرئيسيه</a></li>
-        <br>
-        <li><a href="../الخدمات/government.html">خدمات حكوميه</a></li> 
-        <li><a href="../الخدمات/travel-permit.html">جواز سفر</a></li>
-        <li><a href="../الخدمات/visas.html">تاشيرات</a></li>
-        <li><a href="../الخدمات/hotel.html">حجز فنادق</a></li>
-        <li><a href="../الخدمات/flight.html">حجز طيران</a></li>
-        <li><a href="../الخدمات/tasahel.html">حجز موعد تسهيل</a></li>
-        <hr>
-        <li><a href="../about.html">من نحن</a></li>
-        <li><a href="../content.html">تواصل معنا</a></li>
-    </ul>
-`;
-nav.insertAdjacentHTML("beforeend", ulNavber);
-
-header.append(nav);
-}
-
+section.prepend(span, img)
+header.prepend(section)
 ////////////////////////////////////////
 spanIcon.addEventListener("click", function (e) {
     e.stopPropagation();  
